0091045608c016b2916424141d8f98788b2ef5ab
Chris Heald <cheald@gmail.com>
2019-06-04 23:40:32 -0700

    Compute energy generation at time of day as a function of the beta PDF, normalized to a monthly energy distribution taken from nrel.gov for the Phoenix area

--- lib/plans/solar_base.rb
+++ lib/plans/solar_base.rb
@@ -2,8 +2,8 @@ require_relative "../sun_times"
 
 module Plans
   class SolarBase < ::Plans::Base
-    EFF_BY_MO = [0, 4.4, 5.4, 6.4, 7.5, 8.0, 8.1, 7.5, 7.3, 6.8, 6.0, 4.9, 4.2]
-    EFF_BY_MO_MAX = EFF_BY_MO.max.to_f
+    REL_MONTH_EFF = [0, 916, 918, 1185, 1330, 1358, 1305, 1222, 1209, 1128, 1099, 939, 861]
+    OBS_MONTH_EFF = [0, 852, 947, 1142, 1185, 1324, 1447, 1298, 1268, 1136, 1076, 960, 950]
 
     def self.solar_eligible
       true
@@ -21,26 +21,52 @@ module Plans
       format "$%2.0f", cost_per_watt_installed * system_size * 1000.0
     end
 
+    def beta_pdf(x, a, b)
+      return 0 if x < 0 || x > 1
+
+      gab = Math.lgamma(a + b).first
+      ga = Math.lgamma(a).first
+      gb = Math.lgamma(b).first
+
+      if x == 0.0 || x == 1.0
+        Math.exp(gab - ga - gb) * x ** (a - 1) * (1 - x) ** (b - 1)
+      else
+        Math.exp(gab - ga - gb + Math.log(x) * (a - 1) + log1p(-x) * (b - 1))
+      end
+    end
+
+    def log1p(x)
+      # in C, this is volatile double y.
+      # Not sure how to reproduce that in Ruby.
+      y = 1 + x
+      Math.log(y) - ((y - 1) - x).quo(y) # cancel errors with IEEE arithmetic
+    end
+
     def offset(date, time, kwh)
       offset = 0
       system_size = @options.fetch(:offset, 0).to_f
-      month_modifier = EFF_BY_MO[date.month] / EFF_BY_MO_MAX
 
       efficiency = @options[:efficiency]
       system_size *= efficiency
 
       rise = SunTimes.new.rise(time, @options[:lat], @options[:long]).localtime
       set = SunTimes.new.set(time, @options[:lat], @options[:long]).localtime
+
       set += 86400 if set < rise
       set -= 86400 if set - rise > 86400
 
-      full_strength_window = 3.5 * 3600
-      if time >= rise + full_strength_window && time <= set - full_strength_window
-        offset = system_size * month_modifier
-      elsif time >= rise && time <= set
+      if time >= rise && time <= set
+        # beta chosen by rough experimentation such that the annual output curve roughly matches
+        # the values from https://pvwatts.nrel.gov/pvwatts.php
+        beta = 4
+        offset = 1.05
+        day_p = (time.to_i - rise.to_i) / (set.to_i - rise.to_i).to_f
+        dv = beta_pdf(day_p, beta * offset, beta)
+        dm = beta_pdf(0.5 * offset, beta * offset, beta)
+        pct = dv / dm * 0.85 * (REL_MONTH_EFF[time.month] / OBS_MONTH_EFF[time.month].to_f).to_f
+        # @logger.debug [rise, time, set, day_p]
         # Linear interpolation of efficiency up to the full-strength window
-        percent = ([time - rise, set - time].min / 3600.0) / (full_strength_window / 3600).to_f
-        offset = system_size * month_modifier * percent
+        offset = system_size * pct
       end
       kwh - offset
     end
